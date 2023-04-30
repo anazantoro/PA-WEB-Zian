3cf9aa66156c7b23a13ce6e568decc1103d8036f
Bayhere <102432826+Bay1510@users.noreply.github.com>
2023-04-30 16:36:32 +0800

    added footer

--- index.php
+++ index.php
@@ -144,7 +144,7 @@ $pktc = paket();
                     <div class="container">
                         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                             <div class="col-md-4 d-flex align-items-center">
-                                <span class="text-muted">&copy; 2022 CleanCars</span>
+                                <span class="text-muted">&copy; 2023 CleanCars</span>
                             </div>
                         </footer>
                     </div>
